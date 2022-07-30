9945d17fdd05272daf089c8c77be905c6f0ad9b0
PriyanshiKanani <108862312+PriyanshiKanani@users.noreply.github.com>
2022-07-30 17:40:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace _27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo f = new FileInfo("D:/Radha/C#");
+            Console.WriteLine(" size of a file : " + f.Length.ToString());
+
+
+        }
+    }
+}
